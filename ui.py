5148e83730b9c00aa5e7dcf670b14ebd5e285430
Arnout Luinge <arnout.luinge@gmail.com>
2022-10-15 22:05:54 +0200

    fixed choice menu

--- ui.py
+++ ui.py
@@ -1,12 +1,12 @@
 import organizer
-print("Enter ical link:")
+print("Welcome, to start please enter ical link:")
 ical_src = input()
 print("Using", ical_src, "as input.")
 print("How to sort events?")
 print("1: Lecture, practical, misc")
-
 sort_choice = input()
+print("Creating calendars...")
 
-if sort_choice == 1:
+if sort_choice == "1":
     sort_keywords =  ["Exam", "Lecture", "Project", "Practical"]
     organizer.organise(ical_src, sort_keywords)
