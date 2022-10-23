a2aae67f5f4a5e1e8725af9b03c375a3abfc7497
Arnout Luinge <arnout.luinge@gmail.com>
2022-10-23 15:25:48 +0200

    cleaned up a bit

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ from os import path
 #check wether the config file exists, if not make one
 if not path.exists("config.yml"):
     print("No config file found, lets create one")
-    create_cfg.make_cfg()
+    if create_cfg.make_cfg():
+        print("Succesfully created config file.")
 
+#check wether the dir for the new calendars exists
 if not os.path.exists("new_calendars"):
     os.makedirs("new_calendars")
+    print("Created new_calendars folder.")
 
-# Read YAML file
+#read the YAML config file
 with open("config.yml", 'r') as stream:
     config_data = yaml.safe_load(stream)
 
@@ -29,6 +32,7 @@ def get_file(ical_link):
     cal = open("temp_calendar.ics", 'rb')
     return cal
 
+#get the calendar, make new organised calendars
 def organise(ical_link, sorting_keywords):
     org_cal = get_file(ical_link)
     cal = Calendar.from_ical(org_cal.read())
@@ -47,15 +51,7 @@ def organise(ical_link, sorting_keywords):
         for component in cal.walk():
             if component.name == "VEVENT":
                 if "Type: " + sorting_keywords[i] in component.get('description'):
-                    event = Event()
-                    event.add('summary', sorting_keywords[i] + " " + component.get('summary'))
-                    event.add('dtstart', component.get('dtstart'))
-                    event.add('dtend', component.get('dtend'))
-                    event.add('dtstamp', component.get('dtstamp'))
-                    event.add('location', component.get('location'))
-                    event.add('description', isolate_staff(component.get('description')))
-                    event.add('priority', i)
-                    Temp_cal.add_component(event)
+                    Temp_cal.add_component(create_event(component, i, sorting_keywords))
                 # else:
                 #     event = Event()
                 #     event.add('summary', component.get('summary'))
@@ -67,21 +63,22 @@ def organise(ical_link, sorting_keywords):
                 #     event.add('priority', 10)
                 #     Misc_cal.add_component(event)
 
-        f = open('new_calendars/' + sorting_keywords[i] + '_cal.ics', 'wb')
-        f.write(Temp_cal.to_ical())
-        f.close()
-        print(sorting_keywords[i] + "_cal file created")
+        new_cal = open('new_calendars/' + sorting_keywords[i] + '_cal.ics', 'wb') #create and open the new ical file
+        new_cal.write(Temp_cal.to_ical()) #write the created calendar to the file
+        new_cal.close()
+        print(sorting_keywords[i] + "_cal.ics created")
         i +=1
 
 
-    f = open('new_calendars/Misc_cal.ics', 'wb')
-    f.write(Misc_cal.to_ical())
-    f.close()
-    print("Misc_cal file created")
+    new_cal = open('new_calendars/Misc_cal.ics', 'wb')
+    new_cal.write(Misc_cal.to_ical())
+    new_cal.close()
+    print("Misc_cal.ics created")
 
     org_cal.close()
     return 0
 
+#create a new description with only the staff members
 def isolate_staff(org_desc):
     if 'Staff member(s):' in org_desc:
         start = org_desc.find('Staff member(s):')
@@ -90,6 +87,20 @@ def isolate_staff(org_desc):
         end = org_desc.find('\n')
         return org_desc[:end]
 
+#create the new event by copying most properties of original cal, and alter some other
+def create_event(component, i, sorting_keywords):
+    event = Event()
+    event.add('summary', sorting_keywords[i] + " " + component.get('summary'))
+    event.add('dtstart', component.get('dtstart'))
+    event.add('dtend', component.get('dtend'))
+    event.add('dtstamp', component.get('dtstamp'))
+    event.add('location', component.get('location'))
+    event.add('description', isolate_staff(component.get('description')))
+    event.add('priority', i)
+    return event
+
+#call the main function to run the organizing of calendar
 organise(config_data['Calendar_URL'], config_data['keywords'])
 
+#clean up the temponary downloaded src calendar
 os.remove("temp_calendar.ics")
